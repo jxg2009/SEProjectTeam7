6e3e46378f0d75f40d65467b86e34f43fc6898fd
jxg2009 <jxg2009@nate.com>
2017-11-13 21:54:45 +0900

    Assignment 1

--- Assignment1/main.java
+++ Assignment1/main.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		String cmd;
 		Scanner scan =  new Scanner(System.in);
 		
+		System.out.println("Enter your command");
 		cmd = scan.nextLine();
 		
+		// split cmd
+		
+		// check first word of the command line
+		
+		// help
+		
+		// quit
+		
+		// cvt
 		
 
 	}
