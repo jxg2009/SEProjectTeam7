d99e5e7c76902eb8482e0c735b39ee8d67961d17
jxg2009 <jxg2009@nate.com>
2017-11-13 21:42:57 +0900

    assignment1

---
+++ Assignment1/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cmd;
+		Scanner scan =  new Scanner(System.in);
+		
+		cmd = scan.nextLine();
+		
+		
+		System.out.println("Finish");
+
+	}
+
+}
